587d4434da5f84fc0d346b7ddb2da7267c7d2444
Jhan Bayer <jhanbayer@gmail.com>
2022-04-12 16:12:08 -0400

    fetch data from the component Inicio

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const Inicio = () => {
+  const [clientes, setClientes] = useState([]);
+
+  useEffect(() => {
+    const obtenerClientesAPI = async () => {
+      try {
+        const url = "http://localhost:4000/clientes";
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setClientes(resultado);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    obtenerClientesAPI();
+  }, []);
   return (
     <div>
       <h1>Inicio</h1>
