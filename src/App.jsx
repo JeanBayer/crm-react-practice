efc149eccb8b274090aed2e9c61c24a70eb0f512
Jhan Bayer <jhanbayer@gmail.com>
2022-04-12 11:34:00 -0400

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Hola Mundo h1</h1>
+    </div>
+  );
+}
+
+export default App;
