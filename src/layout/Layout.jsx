abf8475c0dd05788859d03ce8285cbc7f5ca379a
Jhan Bayer <jhanbayer@gmail.com>
2022-04-12 12:51:12 -0400

    nav bar added

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,8 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const location = useLocation();
+  const urlActual = location.pathname;
   return (
     <div className="md:flex md:min-h-screen">
       <div className="md:w-1/4 bg-blue-900 px-5 py-10">
@@ -8,18 +10,22 @@ const Layout = () => {
           CRM - Clientes
         </h2>
         <nav className="mt-10">
-          <a
-            href="/clientes"
-            className="text-white text-2xl block mt-2 hover:text-blue-300"
+          <Link
+            to="/clientes"
+            className={`${
+              urlActual === "/clientes" ? "text-blue-300" : "text-white"
+            }  text-2xl block mt-2 hover:text-blue-300`}
           >
             Clientes
-          </a>
-          <a
-            href="/clientes/nuevo"
-            className="text-white text-2xl block mt-2 hover:text-blue-300"
+          </Link>
+          <Link
+            to="/clientes/nuevo"
+            className={`${
+              urlActual === "/clientes/nuevo" ? "text-blue-300" : "text-white"
+            }  text-2xl block mt-2 hover:text-blue-300`}
           >
             Nuevo Cliente
-          </a>
+          </Link>
         </nav>
       </div>
       <div className="md:w-3/4">
