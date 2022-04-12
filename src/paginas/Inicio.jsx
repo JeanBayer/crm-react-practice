9d41203085a2c73de6b726b03f76f8d9906d8b5c
Jhan Bayer <jhanbayer@gmail.com>
2022-04-12 16:44:10 -0400

    add the functionality to view the records in a table

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 
+import Cliente from "../components/Cliente";
+
 const Inicio = () => {
   const [clientes, setClientes] = useState([]);
 
@@ -17,9 +19,25 @@ const Inicio = () => {
     obtenerClientesAPI();
   }, []);
   return (
-    <div>
-      <h1>Inicio</h1>
-    </div>
+    <>
+      <h1 className="font-black text-4xl text-blue-900 ">Clientes</h1>
+      <p className="mt-3">Administra tus clientes</p>
+      <table className="w-full mt-5 table-auto shadow bg-white">
+        <thead className="bg-blue-800 text-white">
+          <tr>
+            <th className="p-2">Nombre</th>
+            <th className="p-2">Contacto</th>
+            <th className="p-2">Empresa</th>
+            <th className="p-2">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clientes.map((cliente) => (
+            <Cliente key={cliente.id} cliente={cliente}></Cliente>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
